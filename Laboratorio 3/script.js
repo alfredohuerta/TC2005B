2a8ffad780026d316a2f63603933d0d0a3853d55
Pantro Huerta <pantro12@live.com.mx>
2022-02-17 22:09:39 -0600

    Se agregaron preguntas y respuestas en el archivo CSS

---
+++ Laboratorio 3/script.js
@@ -0,0 +1,12 @@
+let personaje= "Mario"; //let es para declarar variables de formas seguras
+
+console.log(personaje);
+console.info(personaje);
+
+for(let i= 0; i<=10; i++){
+    console.log(1);
+}
+
+alert("esto es una alerta");
+
+const nombre= prompt("Quien eres?");

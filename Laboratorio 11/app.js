9e570f02e2a407ad1e8a102a650a369b459aa60f
Pantro Huerta <pantro12@live.com.mx>
2022-03-01 10:25:56 -0600

    Se creó un ejemplo y se agregó el body parser

---
+++ Laboratorio 11/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+//Middleware
+app.use('/ruta', (request, response, next) => {
+    console.log('respuesta de la ruta /ruta')
+    response.send('Hola, esta es la ruta /ruta!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.send('¡Hola mundo!'); //Manda la respuesta
+});
+
+app.listen(3000);

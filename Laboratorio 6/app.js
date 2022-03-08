a4f4b0f93e09ec3107517337cf359ce394d1c826
Pantro Huerta <pantro12@live.com.mx>
2022-03-08 15:47:53 -0600

    Se agregó botón para cambiar de página y tabla de productos

--- Laboratorio 6/app.js
+++ Laboratorio 6/app.js
@@ -4,6 +4,7 @@ let password = "TC2005B";
 function validar() {
     if(prueba1.value === password){
         alert("Contraseña válida")
+        document.getElementById('tienda').style.visibility = 'visible';
     }else if (prueba1.value !== password){
         alert("Contraseña Inválida")
     }

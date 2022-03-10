4ad6dde869a79857ff9431b0c5867b0e3a2ae4fe
Pantro Huerta <pantro12@live.com.mx>
2022-03-09 20:00:28 -0600

    Cambios a las tabulaciones y se agregó una barra de navegación

--- Laboratorio 6/app.js
+++ Laboratorio 6/app.js
@@ -1,6 +1,8 @@
 let prueba1 = document.getElementById('password1');
 let password = "TC2005B";
 
+let item1 = document.getElementById('');
+
 function validar() {
     if(prueba1.value === password){
         alert("Contraseña válida")
@@ -9,3 +11,4 @@ function validar() {
         alert("Contraseña Inválida")
     }
 }
+
